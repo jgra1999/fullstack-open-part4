e49df0ab9bbcbb615b2b38d6b1a783b38bb8c2fa
Jose Rojas <jgra11.2010@gmail.com>
2025-05-07 22:53:22 -0400

    initialize project with package.json and dependencies

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number
+})
+
+blogSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	}
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl =
+	'mongodb+srv://helsinski_fullstack:VB2clWRmQoMhEbKK@cluster0.puvyb.mongodb.net/BlogApp?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose
+	.connect(mongoUrl)
+	.then(() => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+	Blog.find({}).then((blogs) => {
+		response.json(blogs)
+	})
+})
+
+app.post('/api/blogs', (request, response) => {
+	const blog = new Blog(request.body)
+
+	blog.save().then((result) => {
+		response.status(201).json(result)
+	})
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+	console.log(`Server running on port http://localhost:${PORT}`)
+})
