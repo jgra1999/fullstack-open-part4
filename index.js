048413ece67f49c8b0520419d1c0ff96cc6a44e0
Jose Rojas <jgra11.2010@gmail.com>
2025-05-08 01:03:14 -0400

    refactor: move API logic to app.js for better organization

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs)
-	})
-})
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog.save().then((result) => {
-		response.status(201).json(result)
-	})
-})
+const app = require('./app')
 
 const PORT = 3003
 app.listen(PORT, () => {
